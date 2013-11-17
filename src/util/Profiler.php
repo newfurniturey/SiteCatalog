f1ac2d48ac897058c27bd9256251f5c87136ec70
newfurniturey <newfurniturey@gmail.com>
2013-11-17 18:34:26 -0500

    Filled in basic start / stop functionality for the Profiler.

--- src/util/Profiler.php
+++ src/util/Profiler.php
@@ -39,7 +39,16 @@ class Profiler {
 	 * @param String $timer The name of the timer to reset.
 	 */
 	public static function reset($timer) {
-		
+		self::$_timers[$timer] = array(
+			// list of ever run (start/end time) for this timer
+			'runs' => array(),
+			
+			// number of times this timer has been started
+			'num_starts' => 0,
+			
+			// total duration this timer has ran for
+			'total_duration' => 0
+		);
 	}
 
 	/**
@@ -48,15 +57,45 @@ class Profiler {
 	 * @param string $timer The name of the timer to start.
 	 */
 	public static function start($timer) {
+		// if the timer doesn't exist yet, create it
+		if (!isset(self::$_timers[$timer])) {
+			self::reset($timer);
+		}
 		
+		// add the current time as the starting-time for this timer
+		self::$_timers[$timer]['runs'][self::$_timers[$timer]['num_starts']] = array(
+			'start' => microtime(true),
+			'end' => false
+		);
+		self::$_timers[$timer]['num_starts']++;
 	}
 
 	/**
 	 * Stops the specified timer.
 	 *
 	 * @param string $timer The name of the timer to stop.
+	 * @return bool         true if the timer was stopped; otherwise false
 	 */
 	public static function stop($timer) {
+		// grab the end-time now so we don't waste time running misc validation
+		$endTime = microtime(true);
 		
+		// check if the timer actually exists
+		if (!isset(self::$_timers[$timer])) {
+			return false;
+		}
+		
+		$timer = &self::$_timers[$timer];
+		$index = $timer['num_starts'] - 1;
+		
+		// check if the timer is actually running
+		if (($index < 0) || !empty($timer['runs'][$index]['end'])) {
+			return false;
+		}
+		
+		// stop the timer =]
+		$timer['runs'][$index]['end'] = $endTime;
+		$timer['total_duration'] += ($endTime - $timer['runs'][$index]['start']);
+		return true;
 	}
 }
