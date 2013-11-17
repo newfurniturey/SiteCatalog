c1e2c30cb461f8c5447ea8ffe8d2eb18d1a72687
newfurniturey <newfurniturey@gmail.com>
2013-11-17 17:55:35 -0500

    Added CLI validation.

--- src/SiteCatalog.php
+++ src/SiteCatalog.php
@@ -7,3 +7,13 @@
 
 // load the app's config
 require($_SERVER['DOCUMENT_ROOT'] . '/config/bootstrap.php');
+
+// validate that we're running in a console and we have the correct arguments
+// @todo: move this to a dedicated utility
+if (php_sapi_name() != 'cli') {
+	echo 'This application must be executed from a command line.';
+	exit(1);
+} else if ($argc == 1) {
+	echo sprintf("usage: %s domain\n\n", $argv[0]);
+	exit(1);
+}
