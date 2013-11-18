f001393f2db2c68314ef6c2c84e5adac27d23d16
newfurniturey <newfurniturey@gmail.com>
2013-11-17 20:06:44 -0500

    Added an outer timer and stats-display.

--- src/SiteCatalog.php
+++ src/SiteCatalog.php
@@ -5,6 +5,7 @@
  * An automated and interactive tool for cataloging web applications.
  */
 namespace SiteCatalog;
+use SiteCatalog\util\Profiler as Profiler;
 
 // load the app's config
 require('config/bootstrap.php');
@@ -17,4 +18,15 @@ if (php_sapi_name() != 'cli') {
 } else if ($argc == 1) {
 	echo sprintf("usage: %s domain\n\n", $argv[0]);
 	exit(1);
+}
+
+// start our primary timer
+Profiler::start('SiteCatalog');
+
+// @todo: actually write code =P
+
+// we're done! let's display some stats
+if (Profiler::stop('SiteCatalog') && ($runStats = Profiler::get('SiteCatalog'))) {
+	echo "\n\n----\n";
+	echo sprintf("Completed in %.2fs.\n", $runStats['total_duration']);
 }
