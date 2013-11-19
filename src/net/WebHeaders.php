b33f4b2dd8bb37e5d432520ced3eb900a4631db4
newfurniturey <newfurniturey@gmail.com>
2013-11-18 22:31:24 -0500

    Created a list of common web-headers to use in web-requests.

---
+++ src/net/WebHeaders.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Common web headers for convenience access.
+ */
+namespace SiteCatalog\net;
+
+class WebHeaders {
+	const ACCEPT = 'Accept';
+	const CONNECTION = 'Connection';
+	const CONTENT_LENGTH = 'Content-Length';
+	const CONTENT_TYPE = 'Content-Type';
+	const DATE = 'Date';
+	const EXPECT = 'Expect';
+	const HOST = 'Host';
+	const IF_MODIFIED_SINCE = 'If-Modified-Since';
+	const RANGE = 'Range';
+	const REFERER = 'Referer';
+	const TRANSFER_ENCODING = 'Transfer-Encoding';
+	const USER_AGENT = 'User-Agent';
+	const PROXY_CONNECTION = 'Proxy-Connection';
+}
