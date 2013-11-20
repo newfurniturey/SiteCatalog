410ab86970b04b75150332495cdb22c6c9f96e82
newfurniturey <newfurniturey@gmail.com>
2013-11-19 20:19:37 -0500

    Added a shell HttpWebResponse class.

---
+++ src/net/HttpWebResponse.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Provides a HTTP-specific implementation of the WebResponse class.
+ */
+namespace SiteCatalog\net;
+
+class HttpWebResponse extends \SiteCatalog\net\WebResponse {
+	
+}
