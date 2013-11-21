1054221dfaffde8e674b6ed7637e415143495d70
newfurniturey <newfurniturey@gmail.com>
2013-11-21 00:26:25 -0500

    Created two custom exceptions.

---
+++ src/core/exceptions/ArgumentNullException.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Exception for arguments that are passed in with null values when a value is required.
+ */
+namespace SiteCatalog\core\exceptions;
+
+class ArgumentNullException extends \SiteCatalog\core\Exceptions\BaseException {
+	
+}
---
+++ src/core/exceptions/UnsupportedRequestTypeException.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Exception for unsupported WebRequest classes being used.
+ */
+namespace SiteCatalog\core\exceptions;
+
+class UnsupportedRequestTypeException extends \SiteCatalog\core\Exceptions\BaseException {
+	
+}
