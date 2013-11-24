1a15ac383846a097fd2bcf9b17d845c41b4d3449
newfurniturey <newfurniturey@gmail.com>
2013-11-23 23:04:10 -0500

    Added a few additional common headers.

--- src/net/WebHeaders.php
+++ src/net/WebHeaders.php
@@ -7,14 +7,17 @@ namespace SiteCatalog\net;
 class WebHeaders {
 	const Accept = 'Accept';
 	const Connection = 'Connection';
+	const ContentEncoding = 'Content-Encoding';
 	const ContentLength = 'Content-Length';
 	const ContentType = 'Content-Type';
 	const Date = 'Date';
 	const Expect = 'Expect';
 	const Host = 'Host';
 	const IfModifiedSince = 'If-Modified-Since';
+	const LastModified = 'Last-Modified';
 	const Range = 'Range';
 	const Referer = 'Referer';
+	const Server = 'Server';
 	const TransferEncoding = 'Transfer-Encoding';
 	const UserAgent = 'User-Agent';
 	const ProxyConnection = 'Proxy-Connection';
