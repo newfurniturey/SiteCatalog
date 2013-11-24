f78006a7bd063fb5be0c72b960685dcf9c1c6caf
newfurniturey <newfurniturey@gmail.com>
2013-11-24 16:12:13 -0500

    Created a Net utility class with an initial ProcessHeaders() method for processing web headers.

---
+++ src/util/Net.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Provides a static interface for net-related utilities.
+ */
+namespace SiteCatalog\util;
+use SiteCatalog\net\WebHeaderCollection;
+use SiteCatalog\net\WebHeaders;
+
+class Net {
+	
+	/**
+	 * Converts a web-response style string of headers, "\r\n" delimited, into a WebHeaderCollection.
+	 * 
+	 * @param string $strHeaders                    Multi-line string of headers to process.
+	 * @return \SiteCatalog\net\WebHeaderCollection Populated collection of headers.
+	 */
+	public static function ProcessHeaders($strHeaders) {
+		$headerCollection = new WebHeaderCollection();
+		if (empty($strHeaders)) {
+			return $headerCollection;
+		}
+		
+		$headers = explode("\r\n", trim($strHeaders));
+		foreach ($headers as $header) {
+			if (strpos($header, ':') !== false) {
+				// process the name:value header
+				list($name, $value) = explode(':', $header, 2);
+				$headers[trim($name)] = trim($value);
+			} else if (preg_match('/^HTTP\/(1\.[\d])\s+([\d]{3})\s+(.*)?$/i', $header)) {
+				// we have our http status code (with the protocol & message)
+				$headers[WebHeaders::Status] = $header;
+			}
+		}
+		return $headerCollection;
+	}
+	
+}
