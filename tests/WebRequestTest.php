656878bb4791249001b0ef2cdc9488fb7f86d460
newfurniturey <newfurniturey@gmail.com>
2013-11-25 19:00:53 -0500

    Added basic, but not complete, web-request-options testing.

--- tests/WebRequestTest.php
+++ tests/WebRequestTest.php
@@ -36,4 +36,24 @@ class WebRequestTest extends PHPUnit_Framework_TestCase {
 		// see if the request has a response set
 		$this->assertTrue($request->haveResponse);
 	}
+	
+	/**
+	 * @depends testGetResponse
+	 */
+	public function testResponseWithRequestOptions() {
+		// create the request object
+		$uri = 'http://www.php.net';
+		$request = new HttpWebRequest($uri);
+		$request->allowAutoRedirect = false;
+		
+		// get the response and verify that the options we set were translated by the response
+		$response = $request->getResponse();
+		
+		// we disabled auto-redirect so the response-address should be the uri we started with
+		$this->assertEquals($uri, $response->responseUri);
+		
+		// check to see if we actually received contents back
+		// @todo make this check "actual" contents when it's implemented
+		$this->assertTrue($response->contentLength > 0);
+	}
 }
