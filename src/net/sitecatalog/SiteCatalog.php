4b72d9a3fa011b108262d338647f9c495028011a
newfurniturey <newfurniturey@gmail.com>
2013-12-01 17:37:05 -0500

    Refactored SiteCatalog to initiate with a URL instead of a domain.

--- src/net/sitecatalog/SiteCatalog.php
+++ src/net/sitecatalog/SiteCatalog.php
@@ -3,12 +3,18 @@
  * An automated and interactive tool for cataloging web applications.
  */
 namespace net\sitecatalog;
+use util\String;
 
 class SiteCatalog extends \core\Object {
 	/**
-	 * The domain to catalog.
+	 * The base-domain of the originating URL to scan.
 	 */
-	private $_domain = null;
+	private $_baseDomain = null;
+	
+	/**
+	 * The originating URL to scan.
+	 */
+	private $_baseUrl = null;
 	
 	/**
 	 * The options the current cataloging operation will adhere to.
@@ -18,17 +24,33 @@ class SiteCatalog extends \core\Object {
 	/**
 	 * Initializes an instance of the SiteCatalog object.
 	 * 
-	 * @param string $domain The domain to catalog.
+	 * @param string $url    The URL to initiate the site-catalog from.
 	 * @param array $options List of scanning options to use.
 	 * @throws \core\exceptions\ArgumentNullException
 	 */
-	public function __construct($domain, array $options = array()) {
-		if (empty($domain)) {
-			throw new \core\exceptions\ArgumentNullException('$domain');
+	public function __construct($url, array $options = array()) {
+		if (empty($url)) {
+			throw new \core\exceptions\ArgumentNullException('$url');
 		}
 		
-		$this->_domain = $domain;
 		$this->_options = ($options !== null) ? $options : array();
+		$this->_processBaseUrl($url);
 	}
 	
+	/**
+	 * Processes the URL to determine the base domain for the current catalog.
+	 * 
+	 * @param string $url The URL to process.
+	 * @throws \InvalidArgumentException
+	 */
+	private function _processBaseUrl($url) {
+		$parts = String::parse_url($url);
+		
+		if (!is_array($parts) || empty($parts['host'])) {
+			throw new \InvalidArgumentException('$url');
+		}
+		
+		$this->_baseDomain = $parts['host'];
+		$this->_baseUrl = $url;
+	}
 }
