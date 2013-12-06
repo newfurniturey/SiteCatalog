1c2c4e7ed48e4d8a43eab2dab85643f9ca2b4317
newfurniturey <newfurniturey@gmail.com>
2013-12-05 22:20:23 -0500

    Extended the DomDocument constructor to turn off errors and register itself as an extension.

--- src/util/Html.php
+++ src/util/Html.php
@@ -6,4 +6,19 @@ namespace util;
 
 class Html extends \DOMDocument {
 	
+	/**
+	 * Run a few extensions upon initialization.
+	 * 
+	 * @param string $version  The version number of the document as part of the XML declaration.
+	 * @param string $encoding The encoding of the document as part of the XML declaration.
+	 */
+	public function __construct($version = null, $encoding = null) {
+		// disable standard libxml errors
+		libxml_use_internal_errors(true);
+		parent::__construct($version, $encoding);
+		
+		// make sure we extend everything fully
+		$this->registerNodeClass('\DOMDocument', get_called_class());
+	}
+	
 }
