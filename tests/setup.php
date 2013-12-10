330fb2597677e23d608cd8def3906fda898694d6
newfurniturey <newfurniturey@gmail.com>
2013-12-09 22:52:50 -0500

    Defined the tests' root for relative-to-absolute file paths.

--- tests/setup.php
+++ tests/setup.php
@@ -7,6 +7,9 @@ if (!defined('DS')) {
 if (!defined('DOC_ROOT')) {
 	define('DOC_ROOT', dirname(dirname(__FILE__)) . '/src');
 }
+if (!defined('TESTS_ROOT')) {
+	define('TESTS_ROOT', dirname(__FILE__));
+}
 
 // require the main app's bootstrap file for all of the config-requirements
 require_once(DOC_ROOT . '/config/bootstrap.php');
