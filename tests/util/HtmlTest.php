431b14a130b1e0997414e5566ca011de2987aaad
newfurniturey <newfurniturey@gmail.com>
2013-12-09 22:53:10 -0500

    Began creating basic HTML tests and associated files.

---
+++ tests/util/HtmlTest.php
@@ -0,0 +1,32 @@
+<?php
+use util\Html;
+
+require(dirname(dirname(__FILE__)) . '/setup.php');
+class HtmlTest extends PHPUnit_Framework_TestCase {
+	
+	public function testLoadFromFile() {
+		$path = $this->_getHtml('plain', true);
+		
+		$html = new Html();
+		$loaded = $html->loadHTMLFile($path);
+		
+		$this->assertEquals(true, $loaded);
+	}
+	
+	public function testLoadFromString() {
+		$contents = $this->_getHtml('plain');
+		
+		$html = new Html();
+		$loaded = $html->loadHTML($contents);
+		
+		$this->assertEquals(true, $loaded);
+	}
+	
+	private function _getHtml($type, $returnFileName = false) {
+		$path = sprintf('%s/util/HtmlTestDocuments/%s.html', TESTS_ROOT, $type);
+		if (!file_exists($path)) {
+			throw new \InvalidArgumentException('$type');
+		}
+		return $returnFileName ? $path : file_get_contents($path);
+	}
+}
