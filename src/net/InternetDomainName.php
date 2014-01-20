9fbff9007894d51395dc7a3d36cb9af50bed239c
newfurniturey <newfurniturey@gmail.com>
2014-01-20 18:48:56 -0500

    Outlined the basic method skeleton for the InternetDomainName utility.

--- src/net/InternetDomainName.php
+++ src/net/InternetDomainName.php
@@ -4,9 +4,38 @@
  * if a given string is likely to be an addressable domain on the Internet.
  */
 namespace net;
-use net\connection\CurlConnection;
 
-class InternetDomainName {
-
+class InternetDomainName extends PublicSuffixList {
+	
+	/**
+	 * Returns the Public Suffix of the given host.
+	 * 
+	 * @param string $host Host to get the Public Suffix for.
+	 * @return string      Public Suffix, if found.
+	 */
+	public static function getPublicSuffix($host) {
+		return null;
+	}
+	
+	/**
+	 * Returns the sub-domain portion of the given host which is the counter-value
+	 * of InternetDomainName::getTopLevelDomain()
+	 * 
+	 * @param string $host Host to get the subdomain for.
+	 * @return string      Subdomain, if found.
+	 */
+	public static function getSubDomain($host) {
+		return null;
+	}
+	
+	/**
+	 * Returns the top-most domain of the given host including the Public Suffix.
+	 * 
+	 * @param string $host Host to get the TLD for.
+	 * @return string      TLD, if found.
+	 */
+	public static function getTopLevelDomain($host) {
+		return null;
+	}
 	
 }
