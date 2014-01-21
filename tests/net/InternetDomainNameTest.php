9031ea95a72471bbb391497d5c952fff8be52c9f
newfurniturey <newfurniturey@gmail.com>
2014-01-20 19:07:43 -0500

    Added getPublicSuffix() tests.

--- tests/net/InternetDomainNameTest.php
+++ tests/net/InternetDomainNameTest.php
@@ -9,4 +9,18 @@ class InternetDomainNameTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue($refreshed);
 	}
 	
+	public function testSuffixList() {
+		$this->assertEquals(InternetDomainName::getPublicSuffix('biz'), null);
+		$this->assertEquals(InternetDomainName::getPublicSuffix('domain.biz'), 'biz');
+		$this->assertEquals(InternetDomainName::getPublicSuffix('b.domain.biz'), 'biz');
+		$this->assertEquals(InternetDomainName::getPublicSuffix('a.b.domain.biz'), 'biz');
+		$this->assertEquals(InternetDomainName::getPublicSuffix('example.com'), 'com');
+		$this->assertEquals(InternetDomainName::getPublicSuffix('b.example.com'), 'com');
+		$this->assertEquals(InternetDomainName::getPublicSuffix('a.b.example.com'), 'com');
+		$this->assertEquals(InternetDomainName::getPublicSuffix('uk.com'), null);
+		$this->assertEquals(InternetDomainName::getPublicSuffix('example.uk.com'), 'uk.com');
+		$this->assertEquals(InternetDomainName::getPublicSuffix('b.example.uk.com'), 'uk.com');
+		$this->assertEquals(InternetDomainName::getPublicSuffix('a.b.example.uk.com'), 'uk.com');
+		$this->assertEquals(InternetDomainName::getPublicSuffix('test.ac'), 'ac');
+	}
 }
